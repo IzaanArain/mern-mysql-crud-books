d095e6bb674063eddb9301f7e6f04abd28662180
Izzan Saqib <izzan.saqib@binate.pk>
2024-07-12 19:28:58 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const http =  require("http");
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get("/",(req,res) => {
+    res.send("<h1>Server is running</h1>");
+});
+
+server.listen(PORT,() => {
+    console.log(`Server is running on port: ${PORT}`);
+})
